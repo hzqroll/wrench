fb7f240296446c884b6b5841d42d7dc10f3b77cd
zongqiang.hao <zongqiang.hao>
2019-02-27 17:09:20 +0800

    first commit

---
+++ inner/src/main/java/com/roll/wrench/inner/InnerApplication.java
@@ -0,0 +1,13 @@
+package com.roll.wrench.inner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/roll/wrench/WrenchApplication.java
@@ -0,0 +1,13 @@
+package com.roll.wrench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrenchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrenchApplication.class, args);
+    }
+
+}
